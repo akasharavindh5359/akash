d0fb046467a7fda2a0a93885f2dc94d2c5e29738
akasharavindh5359 <130826995+akasharavindh5359@users.noreply.github.com>
2023-04-15 01:18:01 -0700

    Add files via upload

---
+++ Sleep__Tracking/app/src/main/java/com/example/sleep__tracking/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.example.sleep__tracking.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
